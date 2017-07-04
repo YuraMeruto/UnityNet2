73e10b4db396b12c0d6e5113e133b4829cfeb7b9
DESKTOP-PH5IVCD\マサキ <yakmsy.memories@ezweb.ne.jp>
2017-07-04 20:48:14 +0900

    遊ぶステージを作成

---
+++ Assets/Scripts/Game/Board.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Mass;
+
+    void Start()
+    {
+        InstanceBoard();
+    }
+    public void InstanceBoard()
+    {
+        Vector3 InstancePos = Vector3.zero;
+        for(int countx=0;countx<10; countx++)
+        {
+            for (int countz = 0; countz < 10; countz++)
+            {
+                Instantiate(Mass, InstancePos, Quaternion.identity);
+                InstancePos.x += 1.1f;
+            }
+            InstancePos.x = 0;
+            InstancePos.y+=1.1f;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Mass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mass : MonoBehaviour {
+
+    [SerializeField]
+    private List<Material> mass_material= new List<Material>();
+	// Use this for initialization
+	    
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetMaterial(int num)
+    {
+        GetComponent<SpriteRenderer>().material = mass_material[num];
+    }
+}
